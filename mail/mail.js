5adfb242a9c2d6de5ee650656dcb5255d1de773a
Lucas Carvalho Santiago <lucascarvalhosantiago257@gmail.com>
2024-10-25 13:06:43 -0300

    abololo

--- mail/mail.js
+++ mail/mail.js
@@ -1,12 +1,28 @@
-export function authMail(service, user, password) {
-    var nodemailer = require('nodemailer');
-    var transporter = nodemailer.createTransport({
-        service: service,
-        auth: {
-            user: user,
-            password: password,
-        }
+require ('dotenv').config();
+const nodemailer = require('nodemailer');
+
+// Configuração do transportador (neste exemplo, usando o Gmail)
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // Você pode usar outros serviços como 'hotmail', 'yahoo', etc.
+  auth: {
+    user: process.env.EMAIL_USER, // Seu e-mail
+    pass: process.env.EMAIL_PASS // Senha do seu e-mail ou app password para maior segurança
+  }
 });
-userEmail = user;
-}
-export {transporter, userEmail};
+
+// Opções do e-mail
+const mailOptions = {
+  from: process.env.EMAIL_USER, // Endereço de e-mail do remetente
+  to: 'lucascarvalhosantiago257@gmail.com', // Endereço de e-mail do destinatário
+  subject: 'Assunto do e-mail',
+  text: 'Conteúdo do e-mail em texto',
+  html: '<h1>Conteúdo do e-mail em HTML</h1><p>Olá, esse é um e-mail de teste!</p>'
+};
+
+// Enviar o e-mail
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    return console.log('Erro ao enviar o e-mail:', error);
+  }
+  console.log('E-mail enviado:', info.response);
+});
